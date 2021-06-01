8a36ea349e6dbaf0e37ac4d2817eaeb2ecb6a8d1
jakubprzydatek <58308829+jakubprzydatek@users.noreply.github.com>
2021-06-01 11:44:37 +0200

    polish comments added to resolveInstance()

--- src/main/java/SimpleContainer.java
+++ src/main/java/SimpleContainer.java
@@ -44,13 +44,13 @@ public class SimpleContainer {
     private Object resolveInstance(Class<?> classType, HashSet<Class<?>> forbiddenTypes)
             throws IllegalContainerRequest, ClassNotFoundException, InvocationTargetException,
             InstantiationException, IllegalAccessException {
-        Class<?> rootClassType = classType;
+        Class<?> rootClassType = classType; //zapamietujemy klase bazowa na wypadek gdyby typ zarejestrowany mial inny typ do rozwiazania
         boolean objectRegistered = registeredObjects.containsKey(classType);
         boolean singleton = false;
         if(objectRegistered) {
             singleton = registeredObjects.get(classType).getLifeCycle() == ObjectLifeCycle.SINGLETON;
         }
-
+        //sprawdzamy czy aktualny typ wystapil juz w galezi typow do rozwiazania
         if(forbiddenTypes.contains(classType)) {
             throw new IllegalContainerRequest("Cycle occured - object of type " + classType.getTypeName()
                     + " needs object of type " + classType.getTypeName() + "!");
@@ -60,11 +60,13 @@ public class SimpleContainer {
         }
 
         if(objectRegistered && singleton) {
+            //jesli aktualny typ jest zarejestrowany, i jest singletonem to sprawdzamy czy ma przypisana instancje
             if(registeredObjects.get(classType).getInstance() != null) {
-                return registeredObjects.get(classType).getInstance();
+                return registeredObjects.get(classType).getInstance(); // jesli ma przypisana instancje, nie tworzymy nowej a zwracamy juz zapisana
             }
         }
 
+        //jesli aktualny typ nie jest konkretny to musi byc zarejestrowany aby mogl byc rozwiazany
         if(Modifier.isAbstract(classType.getModifiers()) || Modifier.isInterface(classType.getModifiers())) {
             if(objectRegistered) {
                 classType = Class.forName(registeredObjects.get(classType).getType().getTypeName());
@@ -80,6 +82,9 @@ public class SimpleContainer {
         int maxParametersNum = 0;
         int i = 0;
         boolean foundAnnotation = false;
+
+        // przeszukujemy konstruktory podanej klasy, wybieramy takie z adnotacja DependencyConstructor
+        // wpp wybieramy taki z najwieksza liczba parametrow
         for (Constructor<?> constructor: constructors) {
             int paramsNumber = constructor.getParameterTypes().length;
             if(constructor.getAnnotation(DependecyConstructor.class) != null) {
@@ -97,13 +102,15 @@ public class SimpleContainer {
             i++;
         }
         Constructor<?> chosenConstructor = constructors[constructorIndex];
-        Class<?>[] paramsTypes = chosenConstructor.getParameterTypes();
+        Class<?>[] paramsTypes = chosenConstructor.getParameterTypes(); //wyciagamy parametry wybranego konstruktora
         Object[] paramsInstances = new Object[maxParametersNum];
+        // tworzymy kopie zbioru typow, ktore juz sie pojawily i wywolujemy resolve instance na kazdym parametrze konstruktora przekazujac kopie zbioru
         for(int j=0; j<maxParametersNum; j++) {
             HashSet<Class<?>> forbiddenTypesCopy = new HashSet<>(forbiddenTypes);
             paramsInstances[j] = resolveInstance(paramsTypes[j], forbiddenTypesCopy);
         }
-
+        // jezeli podany typ jest zarejestrowany a wczesniej nie mial instancji lub nie jest singletonem(sprawdzilismy taki przypadek na poczatku metody)
+        // to zapisujemy jego instancje do odpowiedniego zarejestowanego typu i zwracamy te instancje
         if(objectRegistered) {
             registeredObjects.get(rootClassType).setInstance(chosenConstructor.newInstance(paramsInstances));
             return registeredObjects.get(rootClassType).getInstance();
